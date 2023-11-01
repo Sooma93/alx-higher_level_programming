84d28ef818bfda4452fdb8a1fd78f8553c966821
sooma93 <tasneemabasit5@gmail.com>
2023-11-01 16:02:11 -0700

    sw

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('Z'), ord('A') - 1, -1):
-        print(f'{chr(c).lower()}' if (ord('z') - c) % 2 == 0 else
-                f'{chr(c)}', end='')
+        print(f'{chr(c).lower()}' if (ord('z') - 
+            c) % 2 == 0 else f'{chr(c)}', end='')
