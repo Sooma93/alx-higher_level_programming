8d5d82e7e307373a6adc3f08345bb66aa0d9fee1
sooma93 <tasneemabasit5@gmail.com>
2023-11-02 12:03:55 -0700

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3def
-magic_calculation(a, b):
-    from magic_calculation_12 import add, sub
+from magic_calculation_12 import add, sub
+def magic_calculation(a, b):
     if a < b:
         i = add(a, b)
         for c in range(4, 6):
