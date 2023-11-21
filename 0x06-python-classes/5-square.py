2a8047d07429d574fd79f09507fe65b040f14a78
sooma93 <tasneemabasit5@gmail.com>
2023-11-21 12:06:55 -0800

    E#

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+define the square class
+"""
+
+
+class Square:
+    """
+    represent square with private size
+    """
+    def __init__(self, size=0):
+        """
+        new instance of square class
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        method for retreving the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set the size attribute
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+
+            self.__size = value
+
+    def area(self):
+        """
+        compute and return value
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        print the square using #
+        """
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
