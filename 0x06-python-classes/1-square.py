a1f96b98fa915dcd8c837263acd81d98213d4b79
sooma93 <tasneemabasit5@gmail.com>
2023-11-21 10:58:24 -0800

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module define the squre class with size
+"""
+
+
+class Square:
+    """
+    represent square
+    """
+    def __init__(self, size):
+        """
+        new squre class
+        """
+        self.__size = size
