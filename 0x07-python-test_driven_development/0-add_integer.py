90ca28ba47019c5e57a71135c6be7e1874ade587
sooma93 <tasneemabasit5@gmail.com>
2023-11-29 07:47:42 -0800

    jjs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,7 +16,4 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
-    return int(a) + int(b)
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/0-add_integer.txt")
+    return a + b
