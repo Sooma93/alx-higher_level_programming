07425453fd06c352ab72984a0a9bbfa8581cebc5
sooma93 <tasneemabasit5@gmail.com>
2023-12-04 08:49:32 -0800

    hjj

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+my list class
+"""
+
+
+class MyList(list):
+    """
+    my list class
+    """
+    def print_sorted(self):
+        """
+        print list
+        """
+        print(sorted(self))
