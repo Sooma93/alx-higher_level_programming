d4168bafab4325142deced94baed561f9c733324
sooma93 <tasneemabasit5@gmail.com>
2023-12-04 08:58:09 -0800

    jjj

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+exact same object
+"""
+
+
+
+def is_same_class(obj, a_class):
+    """
+    define if the object has same instance class
+    """
+    return type(obj) == a_class
