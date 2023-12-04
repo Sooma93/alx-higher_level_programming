5fa5e04853f1a92affaf0c213c93a4ca340099ca
sooma93 <tasneemabasit5@gmail.com>
2023-12-04 09:27:29 -0800

    jjj

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+basegemotry class
+"""
+class BaseGeometry:
+    """
+    basegeometry class
+    """
+    def area(self):
+        """
+        to complete area
+        """
+        raise Exception("area() is not implemented")
