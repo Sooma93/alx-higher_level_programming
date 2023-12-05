379811b6c41df5e7fdee9ac48b0163610547c99e
sooma93 <tasneemabasit5@gmail.com>
2023-12-05 12:48:55 -0800

    class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,18 +17,17 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-            """
-            return dict
-            """
-            try:
-                for attr in attrs:
-                    if type(attr) is not str:
-                        return self.__dict__
-            except Exception:
-                return self.__dict__
-            my_di = dict()
-            for key, value in self.__dict__.items():
-                if key in attrs:
-                    my_di[key]= value
-            return my_di
-
+        """
+        return dict
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_di = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_di[key] = value
+        return my_di
