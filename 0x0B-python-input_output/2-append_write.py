6b3593f5e164c805f03273a4a2f5e721756255c3
sooma93 <tasneemabasit5@gmail.com>
2023-12-05 09:27:36 -0800

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+appened to file
+"""
+def append_write(filename="", text=""):
+    """
+    define append file wit utf8
+    """
+    with open(filename, "a", encoding="UTF8") as f:
+        return f.write(text)
