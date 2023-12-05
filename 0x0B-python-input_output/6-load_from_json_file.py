850d9e34e44005a7145259c13c03e9c8db50d68b
sooma93 <tasneemabasit5@gmail.com>
2023-12-05 11:38:41 -0800

    obj

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+creat object from json file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    define load from json file
+    """
+    with open(filename, "r" encoding"utf-8") as f:
+        return json.load(f)
