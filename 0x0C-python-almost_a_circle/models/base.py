c276b6b018774e05a3947cf6f352c63bdf39005a
sooma93 <tasneemabasit5@gmail.com>
2023-12-11 11:32:08 -0800

    hamz

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,19 @@ class Base:
             list_objs = [o.to_dictionary() for o in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        instance dictionary
+        """
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            i = Rectangle(1, 1)
+        elif cls is Square:
+            i = Square(1)
+        else:
+            i = None
+        i.update(**dictionary)
+        return i
