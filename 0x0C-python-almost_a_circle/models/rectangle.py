6a2405124901fdd5445b1a0fa78f60d279365498
sooma93 <tasneemabasit5@gmail.com>
2023-12-11 09:31:55 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,7 @@ class Rectangle(Base):
         validition value
         """
         if type(value) != int:
-            raise TypeError("{}  must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
