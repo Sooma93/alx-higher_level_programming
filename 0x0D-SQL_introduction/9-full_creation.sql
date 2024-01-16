b02556e73a2f64c66090c7df873bbed12f58bf35
sooma93 <tasneemabasit5@gmail.com>
2024-01-16 12:51:08 -0800

    john clode

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
--- create table
-CREATE TABLE IF NOT EXISTS second_table (id, INT,
+-- create table seconed tablei
+CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
 
