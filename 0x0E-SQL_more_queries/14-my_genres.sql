7bf26a26829043095aaa46c8626cce238b4ca22f
sooma93 <tasneemabasit5@gmail.com>
2024-01-17 13:09:33 -0800

    f

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows
 SELECT g.name
 FROM tv_genres AS g
-INNER JOIN tv_shows_genres AS s
+INNER JOIN tv_show_genres AS s
 ON g.id = s.genre_id
 INNER JOIN tv_shows AS t 
 ON t.id = s. show_id
